3d9a0e4f87c82044787c9417e1ba849089729224
ydongchoi <105weapon@gmail.com>
2025-01-04 01:18:56 -0800

    feat: added url case considering when dev, deploy.

--- BackEnd/src/MiningVehicle/Extensions/ServiceExtensions.cs
+++ BackEnd/src/MiningVehicle/Extensions/ServiceExtensions.cs
@@ -9,22 +9,36 @@ namespace MiningVehicle.Extensions
 {
     public static class ServiceExtensions
     {
-        public static void ConfigureCors(this IServiceCollection services) => 
+        public static void ConfigureCors(this IServiceCollection services) =>
             services.AddCors(options =>
             {
                 options.AddPolicy("AllowReactApp", builder =>
-                builder.WithOrigins("http://localhost:5173") // React app URL
-                .AllowAnyMethod()
-                .AllowAnyHeader()
-                .AllowCredentials());
-            }); 
+                {
+                    var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+                    var reactAppUrl = environment == "Development"
+                        ? "http://localhost:5173"
+                        : "https://happy-river-0f3b0221e.4.azurestaticapps.net";
+
+                    builder.WithOrigins(reactAppUrl)
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials();
+                });
+            });
 
         public static IServiceCollection AddSignalRClients(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddSingleton<HubConnection>(provider =>
             {
+                var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+                var hubUrl = environment == "Development"
+                    ? "http://localhost:5140/vehicleDataHub"
+                    : "https://mining-vehicle.azurewebsites.net/vehicleDataHub";
+
+                Console.WriteLine($"Hub URL: {hubUrl}");
+
                 var hubConnection = new HubConnectionBuilder()
-                    .WithUrl("https://mining-vehicle.azurewebsites.net/vehicleDataHub")
+                    .WithUrl(hubUrl)
                     .WithAutomaticReconnect()
                     .Build();
 
