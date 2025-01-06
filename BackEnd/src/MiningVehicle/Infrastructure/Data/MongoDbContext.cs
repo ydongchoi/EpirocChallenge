7d7a3ab95a5bf29894433881431ed25580d88084
ydongchoi <105weapon@gmail.com>
2025-01-06 03:22:13 -0800

    refactor: MongoDbContext.

--- BackEnd/src/MiningVehicle/Infrastructure/Data/MongoDbContext.cs
+++ BackEnd/src/MiningVehicle/Infrastructure/Data/MongoDbContext.cs
@@ -7,25 +7,21 @@ namespace MiningVehicle.Infrastructure.Data
 {
     public class MongoDbContext
     {
-        private readonly IOptions<MongoDbConfiguration> _mongoDbCongigurationOptions;
-        private readonly MongoDbConfiguration _mongoDbConfiguration;
         private readonly IMongoDatabase _database;
 
         public MongoDbContext(IOptions<MongoDbConfiguration> mongoDBConfigurationOptions)
         {
-            _mongoDbCongigurationOptions = mongoDBConfigurationOptions;
-            _mongoDbConfiguration = _mongoDbCongigurationOptions.Value;
-            var connectionString = Environment.GetEnvironmentVariable("MongoDbConfiguration_ConnectionString");
-            var databaseName = Environment.GetEnvironmentVariable("MongoDbConfiguration_Database");
+            var mongoDbConfiguration = mongoDBConfigurationOptions.Value;
+            var connectionString = Environment.GetEnvironmentVariable("MongoDbConfiguration_ConnectionString") ?? mongoDbConfiguration.ConnectionString;
+            var databaseName = Environment.GetEnvironmentVariable("MongoDbConfiguration_Database") ?? mongoDbConfiguration.Database;
 
             Console.WriteLine($"MongoDB Connection String: {connectionString}");
             Console.WriteLine($"MongoDB Database Name: {databaseName}");
 
-            MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(connectionString));
-            settings.SslSettings = new SslSettings() { EnabledSslProtocols = SslProtocols.Tls12 };
+            var settings = MongoClientSettings.FromUrl(new MongoUrl(connectionString));
+            settings.SslSettings = new SslSettings { EnabledSslProtocols = SslProtocols.Tls12 };
             var client = new MongoClient(settings);
 
-            //var client = new MongoClient(_mongoDbConfiguration.ConnectionString);
             _database = client.GetDatabase(databaseName);
         }
 
