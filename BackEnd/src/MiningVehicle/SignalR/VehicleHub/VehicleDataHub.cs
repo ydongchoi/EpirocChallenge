79551f7289f89798a8bcfd9f506e5ab8a9b9ebeb
ydongchoi <105weapon@gmail.com>
2025-01-06 03:27:38 -0800

    refactor: VehicleDataHub.

--- BackEnd/src/MiningVehicle/SignalR/VehicleHub/VehicleDataHub.cs
+++ BackEnd/src/MiningVehicle/SignalR/VehicleHub/VehicleDataHub.cs
@@ -1,28 +1,35 @@
 using Microsoft.AspNetCore.SignalR;
-using Microsoft.Extensions.Primitives;
 using MiningVehicle.Infrastructure.Repositories;
 using MiningVehicle.SignalR.VehicleHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace MiningVehicle.SignalR.VehicleHub
 {
     public class VehicleDataHub : Hub
     {
         private readonly IRepository _vehicleDataRepository;
-        private List<MiningVehicle.Infrastructure.Models.VehicleData> _vehicleData;
 
         public VehicleDataHub(IRepository vehicleDataRepository)
         {
             _vehicleDataRepository = vehicleDataRepository;
-            _vehicleData = new List<MiningVehicle.Infrastructure.Models.VehicleData>();
         }
 
-        public string GetConnectionId()
-        {
-            return Context.ConnectionId;
-        }
+        public string GetConnectionId() => Context.ConnectionId;
 
         public async Task SendVehicleDataAsync(VehicleData vehicleData)
-        {   
+        {
+            LogVehicleData(vehicleData);
+
+            await SendVehicleDataToUIAsync(vehicleData);
+
+            await Clients.Caller.SendAsync("ReceiveVehicleData", vehicleData);
+            Console.WriteLine("Sent vehicle data to caller...");
+        }
+
+        private void LogVehicleData(VehicleData vehicleData)
+        {
             Console.WriteLine("Received vehicle data...");
             Console.WriteLine($"Timestamp: {vehicleData.Timestamp}");
             Console.WriteLine($"Status: {vehicleData.MotorData.Status}");
@@ -37,63 +44,18 @@ namespace MiningVehicle.SignalR.VehicleHub
             Console.WriteLine($"Battery Percentage: {vehicleData.BatteryData.Percentage}");
             Console.WriteLine($"Battery Power: {vehicleData.BatteryData.Power}");
             Console.WriteLine($"Battery Temperature: {vehicleData.BatteryData.Temperature}");
-
-            // Save vehicle data to database
-            var vehicleDataInfrastructure = new MiningVehicle.Infrastructure.Models.VehicleData
-            {
-                TimeStamp = vehicleData.Timestamp,
-                MotorData = new MiningVehicle.Infrastructure.Models.MotorData
-                {
-                    Status = vehicleData.MotorData.Status,
-                    Rpm = vehicleData.MotorData.Rpm,
-                    Speed = vehicleData.MotorData.Speed,
-                    GearRatio = vehicleData.MotorData.GearRatio
-                },
-                BatteryData = new MiningVehicle.Infrastructure.Models.BatteryData
-                {
-                    Status = vehicleData.BatteryData.Status,
-                    Capacity = vehicleData.BatteryData.Capacity,
-                    Charge = vehicleData.BatteryData.Charge,
-                    ChargingRate = vehicleData.BatteryData.ChargingRate,
-                    Efficiency = vehicleData.BatteryData.Efficiency,
-                    Percentage = vehicleData.BatteryData.Percentage,
-                    Power = vehicleData.BatteryData.Power,                   
-                    Temperature = vehicleData.BatteryData.Temperature
-                },
-                BrakeData = new MiningVehicle.Infrastructure.Models.BrakeData
-                {
-                    Status = vehicleData.BreakData.Status
-                }
-                
-            };
-
-            // Send vehicle data to UI
-            await SendVehicleDataToUIAsync(vehicleData);
-        
-            _vehicleData.Add(vehicleDataInfrastructure);
-            Console.WriteLine($"{_vehicleData.Count} vehicle data saved to list...");
-            if(_vehicleData.Count > 50){
-                await _vehicleDataRepository.AddVehicleDataAsync(_vehicleData);
-                Console.WriteLine("Saved vehicle data to database...");
-                _vehicleData.Clear();
-            }
-            // await _vehicleDataRepository.InsertOneVehicleDataAsync(vehicleDataInfrastructure);
-            // Console.WriteLine("Saved vehicle data to database...");
-       
-            await Clients.Caller.SendAsync("ReceiveVehicleData", vehicleData);
-            Console.WriteLine("Sended vehicle data to caller...");
         }
 
         public async Task SendVehicleDataToUIAsync(VehicleData vehicleData)
-        {   
+        {
             await Clients.All.SendAsync("ReceiveVehicleDataAsync", vehicleData);
-            Console.WriteLine("Sended vehicle data to UI...");
+            Console.WriteLine("Sent vehicle data to UI...");
         }
 
         public Task Ping()
         {
             Console.WriteLine($"Ping received from client at {DateTime.UtcNow}");
             return Task.CompletedTask;
-        }    
+        }
     }
 }
